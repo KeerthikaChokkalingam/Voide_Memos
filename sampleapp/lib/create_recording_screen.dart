fac0c1ae908dbfb72242546fae368fbd6826d2a0
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2025-03-25 20:11:50 +0530

    commit with package setup

--- sampleapp/lib/create_recording_screen.dart
+++ sampleapp/lib/create_recording_screen.dart
@@ -1,19 +1,42 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bounceable/flutter_bounceable.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:record/record.dart';
 
 class CreateRecordingScreen extends StatefulWidget {
 const CreateRecordingScreen({super.key});
 
 @override
-State<CreateRecordingScreen> createState() => _CreateRecordingScreenState();
+State<CreateRecordingScreen> createState() => CreateRecordingScreenState();
 }
 
-class _CreateRecordingScreenState extends State<CreateRecordingScreen> {
+class CreateRecordingScreenState extends State<CreateRecordingScreen> {
 final FocusNode _titleFocusNode = FocusNode();
 
-  @override
-  @override
+final recorder = Record();
+late AudioPlayer _audioPlayer;
+
+var buttenTapped = false;
+String? recordedFilePath;
+Timer? _timer;
+int _elapsedSeconds = 0;
+
+@override
+void initState(){
+  super.initState();
+  _audioPlayer = AudioPlayer();
+}
+@override
+void dispose(){
+  _audioPlayer.dispose();
+  super.dispose();
+}
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFBEFE4),
@@ -27,7 +50,7 @@ final FocusNode _titleFocusNode = FocusNode();
                 topBackButtonTitle(),
                 Padding(
                   padding: const EdgeInsets.only(top: 40.0, left: 10),
-                  child: commontitleTextWidget("Title"),
+                  child: commontitleTextWidget("Title",true),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
@@ -35,7 +58,7 @@ final FocusNode _titleFocusNode = FocusNode();
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 30.0, left: 10),
-                  child: commontitleTextWidget("Topic"),
+                  child: commontitleTextWidget("Topic",true),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
@@ -43,7 +66,7 @@ final FocusNode _titleFocusNode = FocusNode();
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 30.0, left: 10),
-                  child: commontitleTextWidget("Recording"),
+                  child: commontitleTextWidget("Recording",true),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
@@ -118,15 +141,32 @@ Widget topBackButtonTitle() {
     ],
   );
 }
-Widget commontitleTextWidget(String titleText) {
-  return Text(titleText,style: const TextStyle(
-    decoration: TextDecoration.none,
-    fontSize: 16,
-    fontWeight: FontWeight.w800,
-    color: Colors.black,
-
-  ),);
+Widget commontitleTextWidget(String titleText, bool isRequired) {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Text(
+        titleText,
+        style: const TextStyle(
+          decoration: TextDecoration.none,
+          fontSize: 16,
+          fontWeight: FontWeight.w800,
+          color: Colors.black,
+        ),
+      ),
+      if (isRequired) // Only show red star if required
+        const Text(
+          ' *',
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 16,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+    ],
+  );
 }
+
 Widget commonTextFieldWidget(String placeholderText) {
   return Container(
     decoration: BoxDecoration(
@@ -180,19 +220,47 @@ Widget recorderField() {
       borderRadius: BorderRadius.circular(10),
       color: const Color(0xFFECE3D9),
     ),
-    child: const Padding(
+    child: Padding(
       padding: EdgeInsets.only(left: 10.0,right: 10),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("File Audio"),
-          Visibility(
-            visible: false,
-            child: Icon(Icons.delete,
-              color: Color(0xFF5D5FEF),
-              size: 20,),
+          const Text("File Audio"),
+      Row(
+        children: [
+          Bounceable(
+            onTap: () async {
+              if (recordedFilePath != null) {
+                final player = AudioPlayer();
+                await player.setFilePath(recordedFilePath!);
+                player.play();
+                // await _audioPlayer.setFilePath(recordedFilePath!);
+                // _audioPlayer.play();
+                print("Playing recording");
+              }
+            },
+            child: Visibility(
+              visible: recordedFilePath != null,
+              child: Icon(Icons.play_circle, color: Color(0xFF5D5FEF), size: 20),
+            ),
+          ),
+          Bounceable(
+            onTap: () {
+              print("tapped delete");
+            },
+            child: Visibility(
+              visible: recordedFilePath != null,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Icon(Icons.delete,
+                  color: Color(0xFF5D5FEF),
+                  size: 20,),
+              ),
+            ),
           )
+        ],
+      )
         ],
       ),
     )
@@ -210,7 +278,16 @@ Widget startRecording() {
       Padding(
         padding: const EdgeInsets.only(top: 25.0),
         child: Bounceable(
-          onTap: () {  },
+          onTap: () {
+            if (buttenTapped) {
+              _stopRecording();
+            } else {
+              _requestMicrophonePermission();
+            }
+            setState(() {
+              buttenTapped = !buttenTapped;
+            });
+          },
           child: Image.asset(
             'assets/images/record-button.png',
             width: 122,
@@ -218,25 +295,100 @@ Widget startRecording() {
           ),
         ),
       ),
-
+ Padding(padding: const EdgeInsets.only(top: 10.0),
+child: Text(
+  "00:${_elapsedSeconds.toString().padLeft(2, '0')}" ,
+  style: const TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+  ),
+  ),
+ )
     ],
   );
 }
+Future<void> _requestMicrophonePermission() async {
+  final PermissionStatus status = await Permission.microphone.request();
+  if (status == PermissionStatus.granted) {
+    _startRecording();
+    // Permission granted, proceed with recording
+  } else {
+    // Permission denied, handle accordingly (e.g., show a message to the user)
+  }
+}
+Future<void> _startRecording() async {
+  try {
+    if (await recorder.hasPermission()) {
+      // Get the directory to save file
+      final directory = await getApplicationDocumentsDirectory();
+      final filePath = '${directory.path}/myFile_${DateTime.now().millisecondsSinceEpoch}.m4a';
+      print("Recording started. File path: $filePath");
+
+      await recorder.start(
+        path: filePath,
+        encoder: AudioEncoder.aacLc,
+        bitRate: 128000,
+        samplingRate: 44100,
+      );
+      _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        setState(() {
+          _elapsedSeconds++;
+        });
+      });
+    } else {
+      // Handle permission denied
+      print("Recording permission denied.");
+    }
+  } catch (e) {
+    print("Start recording error: $e");
+  }
+}
+
+Future<void> _stopRecording() async {
+  try {
+      final path = await recorder.stop();
+      if (path != null) {
+        print("Recording saved at: $path");
+        _timer?.cancel();
+        setState(() {
+          recordedFilePath = path;
+          _elapsedSeconds = 0;
+        });
+
+    } else {
+      print("Recording failed to save.");
+    }
+  } catch (e) {
+    print("Stop recording error: $e");
+  }
+}
+
+
+
 Widget saveButton(){
-    return Bounceable(onTap: (){}, child: Container(
-      decoration: BoxDecoration(
-        color: const Color(0xFF5D5FEF),
-        borderRadius: BorderRadius.circular(35)
+    return Bounceable(
+      onTap: () {
+        if (recordedFilePath != null) {
+          print("Recording saved at: $recordedFilePath");
+          // You can navigate or store file
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Please record audio before saving")),
+          );
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF5D5FEF),
+          borderRadius: BorderRadius.circular(35),
+        ),
+        height: 51,
+        child: const Center(
+          child: Text("Save", style: TextStyle(color: Colors.white, fontSize: 16)),
+        ),
       ),
-      height: 51,
-      child: const Center(
-        child: Text("Save",style: TextStyle(
-          color:  Colors.white,
-          fontSize: 18,
-          height: 1,
-          fontWeight: FontWeight.w600,
-        ),),
-      ),
-    ));
+    );
+
 }
 }
