0587bb23cc9215e03a2b196c5e4a198e58b1f1ba
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2025-04-26 10:07:24 +0530

    commit with app logo ,name setup

--- sampleapp/test/widget_test.dart
+++ sampleapp/test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:VoiceMemos/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sampleapp/splash_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SplashScreen());
+    await tester.pumpWidget(const MaterialApp(home: SplashScreen()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
